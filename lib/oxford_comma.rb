f6801aa16ccd84c89907a017eda8d2d3fc6aac3e
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 18:24:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
+    array
+  elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
     array[2] = "and " + array[2]
     array.join(", ")
-  else
+  elsif array.length > 3
     array.last = "and " + array.last
     array.join(", ")
   end
